c6dfe248a3f4d4ab96cf97ff1ddb001bd8b6ef49
riddhivasoya <riddhivasoya2097@gmail.com>
2022-04-18 17:02:08 +0530

    add

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -39,8 +39,8 @@ class TagsController extends Controller
         $request->validate([
             'tag_name'=>'required|max:20|unique:tags',
         ]);
-        $tag=$request->all();
-        Tag::create($tag);
+        $input=$request->all();
+        Tag::create($input);
         return redirect()->route('tags.index')
                         ->with('success','Tags created successfully.');
     }
